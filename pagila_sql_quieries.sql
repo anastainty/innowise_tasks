d514bfff07e63fc18ccd745be8575f98029b1b70
Anastasia Intyakova <116066268+anastainty@users.noreply.github.com>
2025-05-17 20:29:46 +0300

    Update pagila_sql_quieries.sql

--- pagila_sql_quieries.sql
+++ pagila_sql_quieries.sql
@@ -16,8 +16,7 @@ SELECT
 FROM
     actor
 JOIN film_actor ON actor.actor_id = film_actor.actor_id
-JOIN film ON film_actor.film_id = film.film_id
-JOIN inventory ON film.film_id = inventory.film_id
+JOIN inventory ON film_actor.film_id = inventory.film_id
 JOIN rental ON inventory.inventory_id = rental.inventory_id
 GROUP BY actor.actor_id, actor.first_name, actor.last_name
 ORDER BY COUNT(rental_id) DESC
